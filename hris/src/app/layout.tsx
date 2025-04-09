ba2fa65b74cab1c8727b82155b0883b03b9efa7d
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-04-09 12:29:25 +0200

    Addec config provider

--- hris/src/app/layout.tsx
+++ hris/src/app/layout.tsx
@@ -1,17 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/providers/auth";
+import { ConfigProvider } from "antd";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,12 +15,62 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <AuthProvider>
+    
       <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable}`}>
-          {children}
-        </body>
+        <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: "#fb9b01",
+            colorInfo: "#fb9b01",
+            sizeStep: 6,
+            borderRadius: 5,
+            wireframe: false,
+            fontSize: 17,
+          },
+          components: {
+            Button: {
+              contentFontSize: 18,
+              contentFontSizeLG: 17,
+              contentFontSizeSM: 0,
+              contentLineHeightLG: 7.5,
+              paddingBlock: 4,
+              controlHeight: 49,
+            },
+            Input: {
+              controlHeight: 48,
+              lineWidth: 2,
+              borderRadius: 15,
+              activeBg: "rgb(33,32,32)",
+            },
+            Layout: {
+              headerHeight: 55,
+              headerBg: "rgb(34,36,38)",
+            },
+            Spin: {
+              dotSize: 57,
+              fontSize: 25,
+              motionDurationMid: "0.1s",
+            },
+            Carousel: {
+              dotHeight: 12,
+            },
+            Select: {
+              controlHeight: 49,
+              borderRadius: 15,
+            },
+            InputNumber: {
+              controlHeight: 48,
+              borderRadius: 15,
+              lineWidth: 2,
+            },
+          },
+        }}
+      >
+        <AuthProvider>
+          <body>{children}</body>
+        </AuthProvider>
+      </ConfigProvider>
       </html>
-    </AuthProvider>
+   
   );
 }
