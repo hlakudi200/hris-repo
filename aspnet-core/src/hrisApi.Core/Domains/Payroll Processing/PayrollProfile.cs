de9f624e2a095ab89c4e14ed169c5a243ae46b17
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-04-10 21:06:26 +0200

    Formatted file

--- aspnet-core/src/hrisApi.Core/Domains/Payroll Processing/PayrollProfile.cs
+++ aspnet-core/src/hrisApi.Core/Domains/Payroll Processing/PayrollProfile.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Abp.Domain.Entities.Auditing;
 using hrisApi.Domains.Employee_Management;
 
@@ -11,14 +8,11 @@ namespace hrisApi.Domains.Payroll_Processing
 {
     public class PayrollProfile : FullAuditedEntity<Guid>
     {
-
         public Guid EmployeeId { get; set; }
         [ForeignKey("EmployeeId")]
         public Employee Employee { get; set; }
         public decimal BasicSalary { get; set; }
-       
         public decimal TaxRate { get; set; }
-
         public ICollection<PayrollTransaction> Transactions { get; set; }
 
     }
