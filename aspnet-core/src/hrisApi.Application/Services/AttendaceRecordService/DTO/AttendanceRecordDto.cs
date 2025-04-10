21e3cfbbd631743fbfd0aeb4193c8ca7931d1b05
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-04-10 17:49:07 +0200

    Created the attendance record app service

---
+++ aspnet-core/src/hrisApi.Application/Services/AttendaceRecordService/DTO/AttendanceRecordDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using hrisApi.Domains.Attendance_Management;
+
+namespace hrisApi.Services.AttendaceRecordService.DTO
+{
+    [AutoMap(typeof(AttendanceRecord))]
+    public class AttendanceRecordDto : EntityDto<Guid>
+    {
+        public Guid EmployeeId { get; set; }
+        public Guid ProjectId { get; set; }
+        public DateTime ClockInTime { get; set; }
+        public DateTime? ClockOutTime { get; set; }
+        public string Note { get; set; }
+    }
+}
