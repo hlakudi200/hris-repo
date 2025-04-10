c1022098f39d3349072b1e066660926a8c537fa8
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-04-11 00:06:38 +0200

    Formatted file

--- aspnet-core/src/hrisApi.Core/Domains/Payroll Processing/Reimbursement.cs
+++ aspnet-core/src/hrisApi.Core/Domains/Payroll Processing/Reimbursement.cs
@@ -1,18 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Abp.Domain.Entities.Auditing;
 
 namespace hrisApi.Domains.Payroll_Processing
 {
     public class Reimbursement : FullAuditedEntity<Guid>
     {
-       
         public Guid PayrollTransactionId { get; set; }
         public decimal Amount { get; set; }
         public string Description { get; set; }
-
     }
 }
