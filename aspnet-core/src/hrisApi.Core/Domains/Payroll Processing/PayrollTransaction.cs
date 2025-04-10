96bd0caaa6362dc97af732f51fdd795f6f52a36f
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-04-10 22:29:18 +0200

    Formatted file

--- aspnet-core/src/hrisApi.Core/Domains/Payroll Processing/PayrollTransaction.cs
+++ aspnet-core/src/hrisApi.Core/Domains/Payroll Processing/PayrollTransaction.cs
@@ -1,15 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Abp.Domain.Entities.Auditing;
 
 namespace hrisApi.Domains.Payroll_Processing
 {
     public class PayrollTransaction : FullAuditedEntity<Guid>
     {
-        
         public Guid PayrollProfileId { get; set; }
         public DateTime PeriodStart { get; set; }
         public DateTime PeriodEnd { get; set; }
@@ -17,9 +13,7 @@ namespace hrisApi.Domains.Payroll_Processing
         public decimal TaxAmount { get; set; }
         public decimal NetAmount { get; set; }
         public bool IsPaid { get; set; }
-
         public ICollection<BonusOrCommission> BonusesAndCommissions { get; set; }
-        //public ICollection<Reimbursement> Reimbursements { get; set; }
-
+        public ICollection<Reimbursement> Reimbursements { get; set; }
     }
 }
