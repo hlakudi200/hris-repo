c6973c9ac56b75e02e4c61c191551efbc9c00cb0
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-04-10 17:11:14 +0200

    Created AbsenceReport service

---
+++ aspnet-core/src/hrisApi.Application/Services/AbsenceReportService/DTO/AbsenceReportDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using hrisApi.Domains.Attendance_Management;
+
+namespace hrisApi.Services.AbsenceReportService.DTO
+{
+    [AutoMap(typeof(AbsenceReport))]
+    public class AbsenceReportDto : EntityDto<Guid>
+    {
+        public Guid EmployeeId { get; set; }
+        public DateTime Date { get; set; }
+        public string AbsenceType { get; set; }
+    }
+}
