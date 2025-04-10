6e1ea8e9c2e241e720826dc81720b12467a4c78b
Rapudi <112486977+hlakudi200@users.noreply.github.com>
2025-04-10 16:42:08 +0200

    Leave Form Component

--- hris/src/app/employee/page.tsx
+++ hris/src/app/employee/page.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 //import Profile from "@/_componets/profileComponent/profile";
-import LeaveOverview from "@/_componets/leaveOverviewComponent/leaveOverview";
+//import LeaveOverview from "@/_componets/leaveOverviewComponent/leaveOverview";
 //import AttandaceOverview from "@/_componets/attandaceComponent/attandanceOverview";
+import LeaveForm from "@/_componets/leaveFormComponet/leaveForm";
 
 const Employee = () => {
-  return <LeaveOverview />;
+  return <LeaveForm />;
 };
 
 export default Employee;
