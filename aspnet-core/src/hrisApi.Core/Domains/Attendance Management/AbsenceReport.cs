05ee0ec7b2128432d7117c04794869c940c89a89
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-04-10 17:10:49 +0200

    Inheriting from FullyAuditedEntity

--- aspnet-core/src/hrisApi.Core/Domains/Attendance Management/AbsenceReport.cs
+++ aspnet-core/src/hrisApi.Core/Domains/Attendance Management/AbsenceReport.cs
@@ -4,17 +4,18 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Abp.Domain.Entities.Auditing;
 using hrisApi.Domains.Employee_Management;
 
 namespace hrisApi.Domains.Attendance_Management
 {
-    public class AbsenceReport
+    public class AbsenceReport : FullAuditedEntity<Guid>
     {
+        public Guid EmployeeId { get; set; }
         [ForeignKey("EmployeeId")]
         public Employee Employee { get; set; }
-        public Guid EmployeeId { get; set; }
         public DateTime Date { get; set; }
-        public string AbsenceType { get; set; } 
+        public string AbsenceType { get; set; }
     }
 
 }
