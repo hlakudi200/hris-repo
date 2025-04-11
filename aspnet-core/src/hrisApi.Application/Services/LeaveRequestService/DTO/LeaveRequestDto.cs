106aa790aa0c6fc2114e6f3b446d9a7c78654177
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-04-11 14:46:01 +0200

    Updated LeaveRequest fields

--- aspnet-core/src/hrisApi.Application/Services/LeaveRequestService/DTO/LeaveRequestDto.cs
+++ aspnet-core/src/hrisApi.Application/Services/LeaveRequestService/DTO/LeaveRequestDto.cs
@@ -10,7 +10,9 @@ namespace hrisApi.Services.LeaveService.DTO
     {
         public Guid EmployeeId { get; set; }
         public string LeaveType { get; set; }
+        public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public string Status { get; set; }
+        public string Reason { get; set; }
     }
 }
