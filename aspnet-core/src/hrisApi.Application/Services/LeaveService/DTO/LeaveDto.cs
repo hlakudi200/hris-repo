5185507a92cadfb50bab321a6733f33abed91376
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-04-11 21:49:42 +0200

    Created leave service

---
+++ aspnet-core/src/hrisApi.Application/Services/LeaveService/DTO/LeaveDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using hrisApi.Domains.Attendance_Management;
+
+namespace hrisApi.Services.LeaveService.DTO
+{
+    [AutoMap(typeof(Leave))]
+    public class LeaveDto : EntityDto<Guid>
+    {
+        public Guid EmployeeId { get; set; }
+        public int Annual { get; set; }
+        public int Sick { get; set; }
+        public int Study { get; set; }
+        public int FamilyResponsibility { get; set; }
+    }
+}
---
+++ aspnet-core/src/hrisApi.Application/Services/LeaveService/LeaveAppService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Abp.Application.Services;
+using Abp.Domain.Repositories;
+using hrisApi.Domains.Attendance_Management;
+using hrisApi.Services.LeaveService.DTO;
+
+namespace hrisApi.Services.LeaveService
+{
+    public class LeaveAppService : AsyncCrudAppService<Leave, LeaveDto, Guid>, ILeaveAppService
+    {
+        public LeaveAppService(IRepository<Leave, Guid> repository) : base(repository)
+        {
+        }
+    }
+}
