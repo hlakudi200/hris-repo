8b89342bab94abe0a20b0ec9c65161ae05f2b17e
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-04-11 08:05:43 +0200

    Created BonusOrCommission app service

---
+++ aspnet-core/src/hrisApi.Application/Services/BonusOrCommissionService/BonusOrCommissionAppService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Abp.Application.Services;
+using Abp.Domain.Repositories;
+using hrisApi.Domains.Payroll_Processing;
+using hrisApi.Services.BonusOrCommissionService.DTO;
+
+namespace hrisApi.Services.BonusOrCommissionService
+{
+    public class BonusOrCommissionAppService : AsyncCrudAppService<BonusOrCommission, BonusOrCommisionDto, Guid>, IBonusOrCommissionAppService
+    {
+        public BonusOrCommissionAppService(IRepository<BonusOrCommission, Guid> repository) : base(repository)
+        {
+        }
+    }
+}
--- aspnet-core/src/hrisApi.Core/Domains/Payroll Processing/BonusOrCommission.cs
+++ aspnet-core/src/hrisApi.Core/Domains/Payroll Processing/BonusOrCommission.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Abp.Domain.Entities.Auditing;
 
 namespace hrisApi.Domains.Payroll_Processing
 {
     public class BonusOrCommission : FullAuditedEntity<Guid>
     {
-        
         public Guid PayrollTransactionId { get; set; }
-        public string Type { get; set; } // Bonus or Commissionpublic decimal Amount { get; set; }
+        public string Type { get; set; } // Bonus or Commission
+        public decimal Amount { get; set; }
         public string Description { get; set; }
     }
 }
