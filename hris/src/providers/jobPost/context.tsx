bf47c0a7ed0e9d446a7f7af82ca174e4c8395b26
Rapudi <112486977+hlakudi200@users.noreply.github.com>
2025-04-12 10:33:14 +0200

    Actions and context

---
+++ hris/src/providers/jobPost/context.tsx
@@ -0,0 +1,33 @@
+'use client'
+import { createContext } from "react";
+
+
+export interface IJobPostingStateContext {
+  isPending: boolean;
+  isSuccess: boolean;
+  isError: boolean;
+  IJobPosting?: IJobPosting;
+  JobPostings?: IJobPosting[];
+}
+
+export interface IJobPostingActionContext {
+  getJobPostings: () => void;
+  getJobPosting: (id: string) => void;
+  createJobPosting: (JobPosting: IJobPosting) => void;
+  updateJobPosting: (JobPosting: IJobPosting) => void;
+  deleteJobPosting: (id: string) => void;
+}
+
+export const INITIAL_STATE: IJobPostingStateContext = {
+  isPending: false,
+  isSuccess: false,
+  isError: false,
+};
+
+export const JobPostingStateContext =
+  createContext<IJobPostingStateContext>(INITIAL_STATE);
+
+export const JobPostingActionContext =
+  createContext<IJobPostingActionContext>(undefined);
+
+  
---
+++ hris/src/providers/jobPost/interfaces.ts
@@ -0,0 +1,36 @@
+interface IInterview {
+    id: string;
+    creationTime: string;
+    creatorUserId: number;
+    lastModificationTime?: string;
+    lastModifierUserId?: number;
+    isDeleted: boolean;
+    deleterUserId?: number;
+    deletionTime?: string;
+    jobApplicationId: string;
+    scheduledDate: string;
+    interviewer: string;
+    mode: string;
+    feedback: string;
+  }
+  
+  interface IJobApplication {
+    id: string;
+    jobPostingId: string;
+    applicantName: string;
+    email: string;
+    resumePath: string;
+    status: string;
+    interviews: IInterview[];
+  }
+  
+  interface IJobPosting {
+    id: string;
+    jobTitle: string;
+    jobDescription: string;
+    location: string;
+    postingDate: string;
+    closingDate: string;
+    status: string;
+    jobApplications ?: IJobApplication[];
+  }
