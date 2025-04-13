2e58c5b4ae255391db6eacdf09ef66ad832d46d3
Rapudi <112486977+hlakudi200@users.noreply.github.com>
2025-04-13 20:46:25 +0200

    log time screen

--- hris/src/app/employee/logHours/page.tsx
+++ hris/src/app/employee/logHours/page.tsx
@@ -1,7 +1,24 @@
-import Profile from "@/_componets/profileComponent/profile";
+import AttendanceForm from "@/_componets/attandanceFormComponent/attandanceForm";
+import styles from "../styles/global.module.css"
+import { Calendar, Card } from "antd";
+import AttendanceOverview from "@/_componets/attandaceComponent/attandanceOverview";
 
 const LogHoursPage: React.FC = () => {
-  return <Profile />;
+  return (
+    <div className={styles.employeeContainer}>
+      <div className={styles.profileSection}>
+      <AttendanceForm/>
+      </div>
+       <div className={styles.calendarSection}>
+        <Card title="Calendar" style={{ width: "100%" }}>
+          <Calendar fullscreen={false} />
+        </Card>
+      </div>
+      <div className={styles.attendanceSection}>
+        <AttendanceOverview/>
+      </div>
+    </div>
+  );
 };
 
 export default LogHoursPage;
