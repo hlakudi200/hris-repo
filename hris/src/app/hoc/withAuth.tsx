df5f7cab08b86a0732fa87387bdfb6136869a44e
Rapudi <112486977+hlakudi200@users.noreply.github.com>
2025-04-14 06:56:56 +0200

    hoc

--- hris/src/app/hoc/withAuth.tsx
+++ hris/src/app/hoc/withAuth.tsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useAuthState } from "@/providers/auth";
+import { Flex, Spin } from "antd";
+import React from "react";
 
-const WithAuth=()=> {
-  return (
-    <div>WithAuths</div>
-  )
+interface IWithAuthProps {
+  allowedRoles?: string[];
 }
 
-export default WithAuth
+const roleMap: Record<number, string> = {
+  1: "admin",
+  2: "employee",
+  3: "hrManager"
+};
+
+const withAuth = (
+  WrappedComponent: React.ComponentType<any>,
+  { allowedRoles = [] }: IWithAuthProps = {}
+) => {
+  const Wrapper: React.FC = (props) => {
+    const { isSuccess, isPending, currentUser, isError } = useAuthState();
+    const router = useRouter();
+
+    useEffect(() => {
+      if (!isPending && currentUser) {
+        const userRoles = currentUser.roles?.map((r) => roleMap[r.roleId]) || [];
+
+        const hasValidRole = allowedRoles.length === 0 || userRoles.some((role) => allowedRoles.includes(role));
+
+        if (isError || !hasValidRole) {
+          if (userRoles.includes("admin")) {
+            router.replace("/hrManager");
+          } else if (userRoles.includes("employee")) {
+            router.replace("/employee");
+          } else {
+            router.replace("/");
+          }
+        }
+      }
+    }, [isPending, isError, currentUser, allowedRoles, router]);
+
+    if (isPending || !isSuccess) {
+      return (
+        <Flex justify="center" style={{ marginBottom: 20 }}>
+          <Spin size="large" />
+        </Flex>
+      );
+    }
+
+    const userRoles = currentUser.roles?.map((r) => roleMap[r.roleId]) || [];
+    const isAuthorized =
+      allowedRoles.length === 0 || userRoles.some((role) => allowedRoles.includes(role));
+
+    return isAuthorized ? <WrappedComponent {...props} /> : null;
+  };
+
+  return Wrapper;
+};
+
+export default withAuth;
