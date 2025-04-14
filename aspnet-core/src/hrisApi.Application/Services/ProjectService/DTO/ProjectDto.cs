795251fde03a061fbfa3c1f6e718e5c765de6999
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-04-14 16:43:08 +0200

    Updated ProjectDto

--- aspnet-core/src/hrisApi.Application/Services/ProjectService/DTO/ProjectDto.cs
+++ aspnet-core/src/hrisApi.Application/Services/ProjectService/DTO/ProjectDto.cs
@@ -5,16 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 using Abp.Application.Services.Dto;
 using Abp.AutoMapper;
+using hrisApi.Domains.Attendance_Management;
 using hrisApi.Domains.Employee_Management;
 
 namespace hrisApi.Services.ProjectService.DTO
 {
-    [AutoMap(typeof(Employee))]
+    [AutoMap(typeof(Project))]
     public class ProjectDto : FullAuditedEntityDto<Guid>
     {
         public string ProjectCode { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public ICollection<Employee>? Employees { get; set; }
     }
 }
