991b2c2a4cb95a5da51565b00914444488b8c4ad
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-04-15 11:32:47 +0200

    Updated styles

--- hris/src/app/employee/logHours/page.tsx
+++ hris/src/app/employee/logHours/page.tsx
@@ -1,21 +1,21 @@
 import AttendanceForm from "@/_componets/attandanceFormComponent/attandanceForm";
-import styles from "./styles/styles.module.css"
+import styles from "./styles/styles.module.css";
 import { Calendar, Card } from "antd";
 import AttendanceOverview from "@/_componets/attandaceComponent/attandanceOverview";
 
 const LogHoursPage: React.FC = () => {
   return (
-    <div className={styles.mainContainer}>
+    <div className={styles.mainContainer} style={{marginTop: -40}}>
       <div className={styles.subSection}>
-      <AttendanceForm/>
+        <AttendanceForm />
       </div>
-       <div className={styles.calendarSection}>
-        <Card title="Calendar" style={{ width: "100%", height: "100%" }}>
-          <Calendar fullscreen={false} />
+      <div className={styles.calendarSection}>
+        <Card title="Calendar" style={{ width: "100%", height: "100%", padding: 0 }}>
+          <Calendar fullscreen={false} style={{ height: 40, marginBottom: 10}} />
         </Card>
       </div>
       <div className={styles.attendanceSection}>
-        <AttendanceOverview/>
+        <AttendanceOverview />
       </div>
     </div>
   );
