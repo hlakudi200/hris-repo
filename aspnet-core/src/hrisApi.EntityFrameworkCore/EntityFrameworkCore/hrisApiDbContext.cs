df7e71509f2d210d0432d767f29c88c42ac0f6e7
Rapudi <112486977+hlakudi200@users.noreply.github.com>
2025-04-16 17:08:07 +0200

    added db context on modal creation

--- aspnet-core/src/hrisApi.EntityFrameworkCore/EntityFrameworkCore/hrisApiDbContext.cs
+++ aspnet-core/src/hrisApi.EntityFrameworkCore/EntityFrameworkCore/hrisApiDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Abp.Zero.EntityFrameworkCore;
+﻿using System.Linq;
+using System;
+using Abp.Zero.EntityFrameworkCore;
 using hrisApi.Authorization.Roles;
 using hrisApi.Authorization.Users;
 using hrisApi.Domains.Attendance_Management;
@@ -33,4 +35,22 @@ public class hrisApiDbContext : AbpZeroDbContext<Tenant, Role, User, hrisApiDbCo
         : base(options)
     {
     }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Force all DateTime and DateTime? to be treated as UTC
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            foreach (var property in entityType.GetProperties()
+                .Where(p => p.ClrType == typeof(DateTime) || p.ClrType == typeof(DateTime?)))
+            {
+                property.SetValueConverter(new Microsoft.EntityFrameworkCore.Storage.ValueConversion.ValueConverter<DateTime, DateTime>(
+                    v => v.Kind == DateTimeKind.Utc ? v : v.ToUniversalTime(),
+                    v => DateTime.SpecifyKind(v, DateTimeKind.Utc)
+                ));
+            }
+        }
+    }
+
 }
