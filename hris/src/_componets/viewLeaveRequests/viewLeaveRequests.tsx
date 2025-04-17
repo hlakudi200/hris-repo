c7be9edef96475b5a37c9477614552482ceef822
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-04-18 01:54:00 +0200

    Initial component

---
+++ hris/src/_componets/viewLeaveRequests/viewLeaveRequests.tsx
@@ -0,0 +1,90 @@
+import { useLeaveRequestActions, useLeaveRequestState } from "@/providers/leaveRequest";
+import { Table, TableColumnsType } from "antd";
+import { useEffect } from "react";
+
+interface DataType {
+    key: string;
+    employeeId: string;
+    startDate: string;
+    endDate: string;
+    leaveType: string;
+    status: string;
+    reason: string;
+  }
+
+
+interface ViewLeaveRequestsProps {
+  employeeId: string;
+}
+
+
+const  ViewLeaveRequests: React.FC<ViewLeaveRequestsProps> = ({
+    employeeId,
+  }) => {
+
+    const { leaveRequests } = useLeaveRequestState();
+    const { getByEmpId } = useLeaveRequestActions();
+
+    useEffect(() => {
+        getByEmpId(employeeId);
+
+    });
+
+    const columns: TableColumnsType<DataType> = [
+        {
+          title: "Start Date",
+          dataIndex: "startDate",
+          key: "startDate",
+          render: (value: string) => {
+            const date = new Date(value);
+            return date.toLocaleDateString("en-Za", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            });
+          },
+        },
+        {
+          title: "End Date",
+          dataIndex: "endDate",
+          key: "endDate",
+          render: (value: string) => {
+            const date = new Date(value);
+            return date.toLocaleDateString("en-Za", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            });
+          },
+        },
+        {
+          title: "Status",
+          dataIndex: "status",
+          key: "status",
+        },
+        {
+          title: "Reason",
+          dataIndex: "reason",
+          key: "reason",
+        },
+    ];
+
+    const dataSource: DataType[] =
+    leaveRequests.map((leaveRequest, index) => ({
+      key: index.toString(),
+      ...leaveRequest,
+    })) || [];
+
+
+    return (
+        <div style={{ padding: "1rem", overflowY: "auto" }}>
+          <Table<DataType>
+            columns={columns}
+            dataSource={dataSource}
+            pagination={{ pageSize: 5 }}
+          />
+        </div>
+      );
+}
+
+export default ViewLeaveRequests;
