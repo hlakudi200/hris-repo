80056f2c5522e8f414000b431c188303a053a691
Rapudi <112486977+hlakudi200@users.noreply.github.com>
2025-04-19 18:13:13 +0200

    end point to get job posting and apllications for the job posts

--- aspnet-core/src/hrisApi.Application/Services/JobPostingService/JobPostingAppService.cs
+++ aspnet-core/src/hrisApi.Application/Services/JobPostingService/JobPostingAppService.cs
@@ -5,16 +5,34 @@ using System.Text;
 using System.Threading.Tasks;
 using Abp.Application.Services;
 using Abp.Domain.Repositories;
+using hrisApi.Domains.Payroll_Processing;
 using hrisApi.Domains.Recruitment_Module;
 using hrisApi.Services.JobPostingService.DTO;
+using hrisApi.Services.PayrollProfileService.DTO;
+using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol.Core.Types;
 
 namespace hrisApi.Services.JobPostingService
 {
     public class JobPostingAppService : AsyncCrudAppService<JobPosting, JobPostingDto, Guid>, IJobPostingAppService
     {
+        private readonly IRepository<JobPosting, Guid> _repository;
         public JobPostingAppService(IRepository<JobPosting, Guid> repository) : base(repository)
         {
+            _repository = repository;
+        }
+
+        public async Task<List<JobPostingDto>> GetAllInclude()
+        {
+            // Load all JobPostings including Applications
+            var jobPostings = await _repository
+                .GetAllIncluding(p => p.Applications)
+                .ToListAsync();
+
+            var result = ObjectMapper.Map<List<JobPostingDto>>(jobPostings);
+
+            return result;
         }
     }
-   
+
 }
