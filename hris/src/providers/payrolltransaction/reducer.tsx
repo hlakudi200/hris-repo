3d2a848946fc45c7d2a9631e587f00bb8f4948aa
Rapudi <112486977+hlakudi200@users.noreply.github.com>
2025-04-19 11:37:21 +0200

    resoveld cors policy

--- hris/src/providers/payrolltransaction/reducer.tsx
+++ hris/src/providers/payrolltransaction/reducer.tsx
@@ -5,132 +5,102 @@ import {
 } from "@/providers/payrolltransaction/context";
 import { PayrollTransactionActionEnums } from "./actions";
 
-export const PayrollTransactionReducer =
-  handleActions<IPayrollTransactionStateContext>(
-    {
-      [PayrollTransactionActionEnums.createPayrollTransactionPending]: (
-        state,
-        action
-      ) => ({
-        ...state,
-        ...action.payload,
-      }),
-      [PayrollTransactionActionEnums.createPayrollTransactionSuccess]: (
-        state,
-        action
-      ) => ({
-        ...state,
-        ...action.payload,
-      }),
-      [PayrollTransactionActionEnums.createPayrollTransactionError]: (
-        state,
-        action
-      ) => ({
-        ...state,
-        ...action.payload,
-      }),
+export const PayrollTransactionReducer = handleActions<IPayrollTransactionStateContext>(
+  {
+    [PayrollTransactionActionEnums.createPayrollTransactionPending]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
 
-      //get payrolltrasaction
+    [PayrollTransactionActionEnums.createPayrollTransactionSuccess]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
 
-      [PayrollTransactionActionEnums.getPayrollTransactionPending]: (
-        state,
-        action
-      ) => ({
-        ...state,
-        ...action.payload,
-      }),
-      [PayrollTransactionActionEnums.getPayrollTransactionSuccess]: (
-        state,
-        action
-      ) => ({
-        ...state,
-        ...action.payload,
-      }),
-      [PayrollTransactionActionEnums.getPayrollTransactionError]: (
-        state,
-        action
-      ) => ({
-        ...state,
-        ...action.payload,
-      }),
+    [PayrollTransactionActionEnums.createPayrollTransactionError]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
 
+    [PayrollTransactionActionEnums.getPayrollTransactionPending]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
 
-      //generate pdf 
-      [PayrollTransactionActionEnums.generatePayrollTransactionPdfPending]: (
+    [PayrollTransactionActionEnums.getPayrollTransactionSuccess]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
 
-      //send payslip
-      [PayrollTransactionActionEnums.sentPaySlipPending]: (
+    [PayrollTransactionActionEnums.getPayrollTransactionError]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
 
-        state,
-        action
-      ) => ({
-        ...state,
-        ...action.payload,
-      }),
+    [PayrollTransactionActionEnums.generatePayrollTransactionPdfPending]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
 
-      [PayrollTransactionActionEnums.generatePayrollTransactionPdfSuccess]: (
+    [PayrollTransactionActionEnums.generatePayrollTransactionPdfSuccess]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
 
-      [PayrollTransactionActionEnums.sentPaySlipSuccess]: (
+    [PayrollTransactionActionEnums.generatePayrollTransactionPdfError]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
 
-        state,
-        action
-      ) => ({
-        ...state,
-        ...action.payload,
-      }),
+    [PayrollTransactionActionEnums.downloadPayrollTransactionPdfPending]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
 
-      [PayrollTransactionActionEnums.generatePayrollTransactionPdfError]: (
+    [PayrollTransactionActionEnums.downloadPayrollTransactionPdfSuccess]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
 
-      [PayrollTransactionActionEnums.sentPaySlipError]: (
+    [PayrollTransactionActionEnums.downloadPayrollTransactionPdfError]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
 
-        state,
-        action
-      ) => ({
-        ...state,
-        ...action.payload,
-      }),
+    [PayrollTransactionActionEnums.sentPaySlipPending]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
 
-      //download pdf 
-      [PayrollTransactionActionEnums.downloadPayrollTransactionPdfPending]: (
-      //sent payslips
-      [PayrollTransactionActionEnums.sentPaySlipsPending]: (
-        state,
-        action
-      ) => ({
-        ...state,
-        ...action.payload,
-      }),
+    [PayrollTransactionActionEnums.sentPaySlipSuccess]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
 
-      [PayrollTransactionActionEnums.downloadPayrollTransactionPdfSuccess]: (
+    [PayrollTransactionActionEnums.sentPaySlipError]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
 
-      [PayrollTransactionActionEnums.sentPaySlipsSuccess]: (
+    [PayrollTransactionActionEnums.sentPaySlipsPending]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
 
-        state,
-        action
-      ) => ({
-        ...state,
-        ...action.payload,
-      }),
+    [PayrollTransactionActionEnums.sentPaySlipsSuccess]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
 
-      [PayrollTransactionActionEnums.downloadPayrollTransactionPdfError]: (
+    [PayrollTransactionActionEnums.sentPaySlipsError]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
 
-      [PayrollTransactionActionEnums.sentPaySlipsError]: (
-
-        state,
-        action
-      ) => ({
-        ...state,
-        ...action.payload,
-      }),
-
-
-      [PayrollTransactionActionEnums.resetStateFlagsAction]: (
-        state,
-        action
-      ) => ({
-        ...state,
-        ...action.payload,
-      }),
-    },
-    INITIAL_STATE
-  );
+    [PayrollTransactionActionEnums.resetStateFlagsAction]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
+  },
+  INITIAL_STATE
+);
