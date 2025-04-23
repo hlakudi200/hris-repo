65feb32546e55787ebb8a84620b2accaf1bebc94
Rapudi <112486977+hlakudi200@users.noreply.github.com>
2025-04-23 05:25:58 +0200

    applicant provider context

---
+++ hris/src/providers/jobApplicant/context.tsx
@@ -0,0 +1,56 @@
+
+'use client';
+import { createContext } from "react";
+
+export interface IJobApplicant {
+    id?: string; 
+    userId: number;
+    nationalIdNo: number;
+    gender: string;
+    dateofBirth: string; 
+    cellphoneNo: string;
+    alternativePhone: string;
+    addressLine1: string;
+    addressLine2: string;
+    city: string;
+    province: string;
+    country: string;
+    postalCode: string;
+    highestQualification: string;
+    fieldOfStudy: string;
+    institution: string;
+    graduationYear: string; 
+    yearsOfExperience: number;
+    currentEmployer: string;
+    currentPosition: string;
+    currentSalary: number;
+    resumeUrl: string;
+    coverletter: string;
+    isWillingToRelocate: boolean;
+    hasCriminalRecord: boolean;
+  }
+  
+  export interface ApplicantStateContext {
+    isPending: boolean;
+    isSuccess: boolean;
+    isError: boolean;
+    applicat?:IJobApplicant
+  }
+  
+  export interface ApplicantActionContext {
+    getApplicantById: (userId: number) => void;
+    createApplicant: (applicant: IJobApplicant) => void;
+    updateApplicant: (Attandance: IJobApplicant) => void;
+  }
+  
+  export const INITIAL_STATE: ApplicantStateContext = {
+    isPending: false,
+    isSuccess: false,
+    isError: false,
+  };
+  
+  export const AttandanceStateContext =
+    createContext<ApplicantStateContext>(INITIAL_STATE);
+  
+  export const AttandanceActionContext =
+    createContext<ApplicantActionContext>(undefined);
