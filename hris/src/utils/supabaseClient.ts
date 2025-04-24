2ec3293c133f3e31a7c8a0265090dcc75cc2ad1f
Rapudi <112486977+hlakudi200@users.noreply.github.com>
2025-04-24 04:06:12 +0200

    configured our env

--- hris/src/utils/supabaseClient.ts
+++ hris/src/utils/supabaseClient.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-//const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-//const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ;
-const supabaseUrl = "https://dyyzaihcveetwseuukkw.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR5eXphaWhjdmVldHdzZXV1a2t3Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NDgxMDY1NywiZXhwIjoyMDYwMzg2NjU3fQ.zRCCkkWQIDQI5KNRre0ar5tAs9wAu_sL7DZozbRAVCk"
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ;
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
